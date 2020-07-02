c0e3a83eaf12408e5358a11a8a43432aa6879ef5
vahidalizadeh525 <67735865+vahidalizadeh525@users.noreply.github.com>
2020-07-02 13:59:43 +0430

    Add files via upload
    uindex

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "Hello git";
+$test == 2;
+if ($){
+echo = "test is three";
+}
+else
+{
+echo = "test is three";
+}
+function looptest1 {
+$i=$a;
+while ($i<$b){
+echo "index: $i\n";
+$i++;
+function looptest1 {
+wqddedevcfsfewsrfd
+hkj
+}
+}
+?>
